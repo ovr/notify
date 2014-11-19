ad3f28596fe0bc7870e6bc506749d1a19bab6151
Patsura Dmitry <zaets28rus@gmail.com>
2014-11-19 18:22:52 +0900

    Added OSX provider

---
+++ src/Provider/OSX.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Patsura Dmitry http://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace PHPSystem\Notify\Provider;
+
+use PHPSystem\Notify\Notification;
+
+class NotifySend implements Provider
+{
+    public function notify(Notification $notification)
+    {
+        exec('display notification "' . $notification->getMessage() . '"');
+    }
+}
